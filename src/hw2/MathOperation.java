2b26b82685ec4286e206f5cfce680f3dc3c9c7da
AleksMulyk <mavpakuusa@gmail.com>
2024-02-16 23:33:39 -0600

    1.1 HW1

---
+++ src/hw2/MathOperation.java
@@ -0,0 +1,23 @@
+package hw2;
+
+public class MathOperation {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 7;
+        System.out.println(a + b);
+        System.out.println(a * b);
+        System.out.println(a - b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+        if (a % 2 == 0) {
+            System.out.println(a + " is even number");
+        } else {
+            System.out.println(a + " is odd number");
+        }
+        if (b % 2 == 0) {
+            System.out.println(b + " is even number");
+        } else {
+            System.out.println(b + " is odd number");
+        }
+    }
+}
