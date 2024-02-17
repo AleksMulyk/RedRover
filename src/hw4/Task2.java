54f179b422e7c1fbd52761e75cf82cb9c3147e64
AleksMulyk <mavpakuusa@gmail.com>
2024-02-17 00:48:07 -0600

    3. HW3

---
+++ src/hw4/Task2.java
@@ -0,0 +1,9 @@
+package hw4;
+
+public class Task2 {
+    public static void main(String[] args) {
+        for (int i = 5; i < 10000; i *= 5) {
+           System.out.println(i);
+        }
+    }
+}
---
+++ src/hw4/Task3.java
@@ -0,0 +1,14 @@
+package hw4;
+
+public class Task3 {
+    public static void main(String[] args) {
+        for (int i = 40; i < 61; i++) {
+            if (i % 4 == 0) {
+                System.out.println(i);
+            }
+        }
+        for (int i = 40; i < 61; i += 4) {
+                System.out.println(i);
+        }
+    }
+}
