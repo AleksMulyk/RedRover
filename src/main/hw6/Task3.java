d5fe29c983baa896408843cf1ecafaf0f25bf550
AleksMulyk <mavpakuusa@gmail.com>
2024-02-19 01:34:15 -0600

    6. HW6

---
+++ src/main/hw6/Task3.java
@@ -0,0 +1,33 @@
+package main.hw6;
+
+/*
+Задача №3
+
+Дан массив:
+String[][] array = {{“Привет”, “всем”, “кто”}, {“изучает”, “язык”, “программирования”}, {“java”}};
+необходимо подсчитать количество строк в массиве, которые не содержат буквы “е”.
+
+ */
+
+public class Task3 {
+    public static void main(String[] args) {
+        String[][] array = {{"Привет", "всем", "кто"}, {"изучает", "язык", "программирования"}, {"java"}};
+        int counter = 0;
+        int num = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                counter = 0;
+                String s = array[i][j];
+                for (int k = 0; k < s.length(); k++) {
+                    if (s.charAt(k) == 'е' ) {
+                        counter++;
+                    }
+                }
+                if (counter == 0) {
+                    num++;
+                }
+            }
+        }
+        System.out.println(num);
+    }
+}
---
+++ src/main/hw6/Task4.java
@@ -0,0 +1,43 @@
+package main.hw6;
+
+/*
+Экстра задача
+
+Дана строка:
+String s = “Посмотрите как Рите нравится ритм”;
+необходимо вывести индексы начала всех подстрок - “рит”, независимо от регистра.
+Для указанной строки ответ будет 6, 15, 29.
+*/
+
+public class Task4 {
+    public static void main(String[] args) {
+        String s = "Посмотрите как Рите нравится ритм";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'р'  && s.charAt(i + 1) == 'и' && s.charAt(i + 2) == 'т') {
+                System.out.print(i + " ");
+            }
+            if (s.charAt(i) == 'Р'  && s.charAt(i + 1) == 'и' && s.charAt(i + 2) == 'т') {
+                System.out.print(i + " ");
+            }
+            if (s.charAt(i) == 'р'  && s.charAt(i + 1) == 'И' && s.charAt(i + 2) == 'т') {
+                System.out.print(i + " ");
+            }
+            if (s.charAt(i) == 'р'  && s.charAt(i + 1) == 'и' && s.charAt(i + 2) == 'Т') {
+                System.out.print(i + " ");
+            }
+            if (s.charAt(i) == 'Р'  && s.charAt(i + 1) == 'И' && s.charAt(i + 2) == 'т') {
+                System.out.print(i + " ");
+            }
+            if (s.charAt(i) == 'Р'  && s.charAt(i + 1) == 'и' && s.charAt(i + 2) == 'Т') {
+                System.out.print(i + " ");
+            }
+            if (s.charAt(i) == 'р'  && s.charAt(i + 1) == 'И' && s.charAt(i + 2) == 'Т') {
+                System.out.print(i + " ");
+            }
+            if (s.charAt(i) == 'Р'  && s.charAt(i + 1) == 'И' && s.charAt(i + 2) == 'Т') {
+                System.out.print(i + " ");
+            }
+
+        }
+    }
+}
